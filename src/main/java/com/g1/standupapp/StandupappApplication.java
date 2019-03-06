ef57f5d8561c77a969936dd2f76d486cb5db954e
cwm <coleman.mchose@agile5technologies.com>
2019-03-06 18:17:20 -0500

    first commit

---
+++ src/main/java/com/g1/standupapp/StandupappApplication.java
@@ -0,0 +1,13 @@
+package com.g1.standupapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandupappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandupappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/g1/standupapp/StandupappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.g1.standupapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StandupappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
