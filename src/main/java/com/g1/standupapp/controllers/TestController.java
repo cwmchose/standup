9d7602a6c55d8c36b12d1e508d0cf91ed4f746a7
coleman <cwmchose@mix.wvu.edu>
2019-03-24 16:34:41 -0400

    about to merge

--- src/main/java/com/g1/standupapp/controllers/TestController.java
+++ src/main/java/com/g1/standupapp/controllers/TestController.java
@@ -1,11 +1,19 @@
+package com.g1.standupapp.controllers;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
 
-    
+   	@RequestMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    } 
 
 }
