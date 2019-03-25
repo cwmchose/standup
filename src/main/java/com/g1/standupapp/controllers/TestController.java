9d06c3b311c5c688bd6e44d601efcbb752f78c05
coleman <cwmchose@mix.wvu.edu>
2019-03-24 20:18:32 -0400

    for ed

--- src/main/java/com/g1/standupapp/controllers/TestController.java
+++ src/main/java/com/g1/standupapp/controllers/TestController.java
@@ -9,11 +9,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
-
+   	@RequestMapping("/test")
+	@ResponseBody
+    public String test(String name, Model model) {
+		System.out.println("swell");
+        return "swell";
+    } 
+/*
    	@RequestMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
     } 
-
+*/
 }
